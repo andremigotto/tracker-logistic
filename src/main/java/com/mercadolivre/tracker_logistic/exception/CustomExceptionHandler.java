7615ed49466de6cac27f97d563c029c921aca6b1
migotto <andremigotto@icloud.com>
2025-01-30 18:43:12 -0300

    feat: Adding more ExceptionHandler on ControllerAdvice

--- src/main/java/com/mercadolivre/tracker_logistic/exception/CustomExceptionHandler.java
+++ src/main/java/com/mercadolivre/tracker_logistic/exception/CustomExceptionHandler.java
@@ -1,8 +1,13 @@
 package com.mercadolivre.tracker_logistic.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.HashMap;
@@ -11,10 +16,21 @@ import java.util.Map;
 @ControllerAdvice
 public class CustomExceptionHandler {
 
+
     @ExceptionHandler(ResponseStatusException.class)
     public ResponseEntity<Map<String, Object>> handleResponseStatusException(ResponseStatusException ex) {
         Map<String, Object> response = new HashMap<>();
         response.put("error", ex.getReason());
         return new ResponseEntity<>(response, ex.getStatusCode());
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<Map<String, Object>> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("error", "Invalid argument type");
+        response.put("message", ex.getMessage());
+        response.put("parameter", ex.getName());
+        response.put("value", ex.getValue());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 }
