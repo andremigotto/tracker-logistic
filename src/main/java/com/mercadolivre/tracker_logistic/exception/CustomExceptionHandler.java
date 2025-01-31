1c614a10606a61e7db2be7c752d29e04f70eb36b
migotto <andremigotto@icloud.com>
2025-01-31 19:29:46 -0300

    feat: Adding DataIntegrityViolation on CustomExceptionHandler

--- src/main/java/com/mercadolivre/tracker_logistic/exception/CustomExceptionHandler.java
+++ src/main/java/com/mercadolivre/tracker_logistic/exception/CustomExceptionHandler.java
@@ -1,12 +1,10 @@
 package com.mercadolivre.tracker_logistic.exception;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -31,6 +29,13 @@ public class CustomExceptionHandler {
         response.put("message", ex.getMessage());
         response.put("parameter", ex.getName());
         response.put("value", ex.getValue());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<Map<String, Object>> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("error", ex.getMessage());
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 }
