b9ca7734edc6efd12b8c736c332d03a8fd4c9ac7
migotto <andremigotto@icloud.com>
2025-02-01 03:05:55 -0300

    feat: Fixing the QueryService and Repository Parcel to not search events in all requisitions on Parsel Database

--- src/main/java/com/mercadolivre/tracker_logistic/repository/ParcelRepository.java
+++ src/main/java/com/mercadolivre/tracker_logistic/repository/ParcelRepository.java
@@ -2,15 +2,24 @@ package com.mercadolivre.tracker_logistic.repository;
 
 import com.mercadolivre.tracker_logistic.domain.parcel.ParcelEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ParcelRepository extends JpaRepository<ParcelEntity, UUID> {
 
-    List<ParcelEntity> findBySender(String sender);
+    @Query("SELECT new com.mercadolivre.tracker_logistic.domain.parcel.ParcelEntity(p.id, p.description, p.sender, p.recipient, p.status, p.createdAt, p.updatedAt) FROM ParcelEntity p WHERE p.sender = :sender AND p.recipient = :recipient")
+    List<ParcelEntity> findBySenderAndRecipientWithoutEvents(@Param("sender") String sender, @Param("recipient") String recipient);
 
-    List<ParcelEntity> findByRecipient(String recipient);
+    @Query("SELECT new com.mercadolivre.tracker_logistic.domain.parcel.ParcelEntity(p.id, p.description, p.sender, p.recipient, p.status, p.createdAt, p.updatedAt) FROM ParcelEntity p WHERE p.sender = :sender")
+    List<ParcelEntity> findBySenderWithoutEvents(@Param("sender") String sender);
 
-    List<ParcelEntity> findBySenderAndRecipient(String sender, String recipient);
+    @Query("SELECT new com.mercadolivre.tracker_logistic.domain.parcel.ParcelEntity(p.id, p.description, p.sender, p.recipient, p.status, p.createdAt, p.updatedAt) FROM ParcelEntity p WHERE p.recipient = :recipient")
+    List<ParcelEntity> findByRecipientWithoutEvents(@Param("recipient") String recipient);
+
+    @Query("SELECT new com.mercadolivre.tracker_logistic.domain.parcel.ParcelEntity(p.id, p.description, p.sender, p.recipient, p.status, p.createdAt, p.updatedAt) FROM ParcelEntity p WHERE p.id = :id")
+    Optional<ParcelEntity> findParcelWithoutEvents(@Param("id") UUID id);
 }
--- src/main/java/com/mercadolivre/tracker_logistic/service/ParcelQueryService.java
+++ src/main/java/com/mercadolivre/tracker_logistic/service/ParcelQueryService.java
@@ -1,10 +1,8 @@
 package com.mercadolivre.tracker_logistic.service;
 
-import com.mercadolivre.tracker_logistic.domain.event.EventEntity;
 import com.mercadolivre.tracker_logistic.domain.parcel.ParcelEntity;
 import com.mercadolivre.tracker_logistic.repository.EventRepository;
 import com.mercadolivre.tracker_logistic.repository.ParcelRepository;
-import com.mercadolivre.tracker_logistic.validation.ParcelValidation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -22,43 +20,25 @@ public class ParcelQueryService {
     @Autowired
     private EventRepository eventRepository;
 
-    //Consulta um pacote pelo seu ID único, incluindo eventos se solicitado
-    public ParcelEntity getParcelById(UUID parcelId, boolean includeEvents) {
-
-        ParcelEntity parcel = ParcelValidation.validateParcelExists(parcelRepository, parcelId);
-
-        if (!includeEvents) {
-            parcel.setEvents(null);
-        }
-
-        return parcel;
+    //Consulta um pacote pelo seu ID único, excluindo eventos se solicitado
+    public ParcelEntity getParcelById(UUID id, boolean includeEvents) {
+        return includeEvents
+                ? parcelRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Parcel not found"))
+                : parcelRepository.findParcelWithoutEvents(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Parcel not found"));
     }
 
     //Consulta uma lista de pacotes com base em filtros de remetente e/ou destinatário se solicitado filtro.
     public List<ParcelEntity> getParcelsByFilter(String sender, String recipient) {
-        List<ParcelEntity> parcels;
+        List<ParcelEntity> parcels =
+                (sender != null && recipient != null) ? parcelRepository.findBySenderAndRecipientWithoutEvents(sender, recipient) :
+                        (sender != null) ? parcelRepository.findBySenderWithoutEvents(sender) :
+                                (recipient != null) ? parcelRepository.findByRecipientWithoutEvents(recipient) :
+                                        parcelRepository.findAll();
 
-        if (sender != null && recipient != null) {
-            parcels = parcelRepository.findBySenderAndRecipient(sender, recipient);
-            if (parcels.isEmpty()) {
-                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No parcels found for the given sender and recipient");
-            }
-        } else if (sender != null) {
-            parcels = parcelRepository.findBySender(sender);
-            if (parcels.isEmpty()) {
-                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No parcels found for sender: " + sender);
-            }
-        } else if (recipient != null) {
-            parcels = parcelRepository.findByRecipient(recipient);
-            if (parcels.isEmpty()) {
-                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No parcels found for recipient: " + recipient);
-            }
-        } else {
-            parcels = parcelRepository.findAll();
-            if (parcels.isEmpty()) {
-                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No parcels found with the provided filters.");
-            }
+        if (parcels.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No parcels found with the provided filters.");
         }
         return parcels;
     }
+
 }
