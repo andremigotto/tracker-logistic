e7312fe30647e41f9a7396d469aa877d5aca8ccf
migotto <andremigotto@icloud.com>
2025-02-01 03:11:17 -0300

    feat: removing double check date on event creation

--- src/main/java/com/mercadolivre/tracker_logistic/service/EventService.java
+++ src/main/java/com/mercadolivre/tracker_logistic/service/EventService.java
@@ -5,13 +5,14 @@ import com.mercadolivre.tracker_logistic.domain.event.EventRecord;
 import com.mercadolivre.tracker_logistic.domain.parcel.ParcelEntity;
 import com.mercadolivre.tracker_logistic.repository.EventRepository;
 import com.mercadolivre.tracker_logistic.repository.ParcelRepository;
-import com.mercadolivre.tracker_logistic.validation.ParcelValidation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.Instant;
 import java.util.concurrent.CompletableFuture;
@@ -32,13 +33,14 @@ public class EventService {
     public void createEvent(EventRecord eventRecord) {
         logger.info("[START] Processando evento para o pacote {}", eventRecord.parcelId());
 
-        ParcelEntity parcel = ParcelValidation.validateParcelExists(parcelRepository, eventRecord.parcelId());
+        ParcelEntity parcel = parcelRepository.findById(eventRecord.parcelId())
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Parcel not found by ID"));
 
         EventEntity event = new EventEntity();
         event.setParcel(parcel);
         event.setLocation(eventRecord.location());
         event.setDescription(eventRecord.description());
-        event.setDateTime(eventRecord.date() != null ? eventRecord.date() : Instant.now());
+        event.setDateTime(eventRecord.date());
 
         eventRepository.save(event);
         logger.info("[END] Evento salvo para o pacote {} na thread {}", eventRecord.parcelId(), Thread.currentThread().getName());
