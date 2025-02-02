f4b3dfaab0cffbf48e08327d7f0398b5379512f9
migotto <andremigotto@icloud.com>
2025-02-02 16:56:55 -0300

    feat: Adding Hiraki configuration on Database for pool connection and resilience

---
+++ src/main/java/com/mercadolivre/tracker_logistic/config/DatabaseConfig.java
@@ -0,0 +1,43 @@
+package com.mercadolivre.tracker_logistic.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfig {
+
+    private final DatabaseConfigProperties hikariProperties;
+
+    public DatabaseConfig(DatabaseConfigProperties hikariProperties) {
+        this.hikariProperties = hikariProperties;
+    }
+
+    @Value("${spring.datasource.url}")
+    private String databaseUrl;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(databaseUrl);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setMaximumPoolSize(hikariProperties.getMaximumPoolSize());
+        dataSource.setMinimumIdle(hikariProperties.getMinimumIdle());
+        dataSource.setConnectionTimeout(hikariProperties.getConnectionTimeout());
+        dataSource.setIdleTimeout(hikariProperties.getIdleTimeout());
+        dataSource.setMaxLifetime(hikariProperties.getMaxLifetime());
+        dataSource.setKeepaliveTime(hikariProperties.getKeepaliveTime());
+        dataSource.setValidationTimeout(hikariProperties.getValidationTimeout());
+        return dataSource;
+    }
+}
---
+++ src/main/java/com/mercadolivre/tracker_logistic/config/DatabaseConfigProperties.java
@@ -0,0 +1,20 @@
+package com.mercadolivre.tracker_logistic.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties(prefix = "spring.datasource.hikari")
+public class DatabaseConfigProperties {
+    private int maximumPoolSize;
+    private int minimumIdle;
+    private int connectionTimeout;
+    private int idleTimeout;
+    private int maxLifetime;
+    private int keepaliveTime;
+    private int validationTimeout;
+}
