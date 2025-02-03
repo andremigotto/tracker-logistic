535454fc294da34ae24f96b05a9dbbb04b51c72c
migotto <andremigotto@icloud.com>
2025-02-03 02:26:30 -0300

    feat: Changing getters and setters to @Getter and @Setter lombok

--- src/main/java/com/mercadolivre/tracker_logistic/config/AsyncProperties.java
+++ src/main/java/com/mercadolivre/tracker_logistic/config/AsyncProperties.java
@@ -1,46 +1,18 @@
 package com.mercadolivre.tracker_logistic.config;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+@Getter
+@Setter
 @Component
 @ConfigurationProperties(prefix = "async")
 public class AsyncProperties {
+    // Getters e Setters
     private int corePoolSize;
     private int maxPoolSize;
     private int queueCapacity;
     private int keepAliveSeconds;
-
-    // Getters e Setters
-    public int getCorePoolSize() {
-        return corePoolSize;
-    }
-
-    public void setCorePoolSize(int corePoolSize) {
-        this.corePoolSize = corePoolSize;
-    }
-
-    public int getMaxPoolSize() {
-        return maxPoolSize;
-    }
-
-    public void setMaxPoolSize(int maxPoolSize) {
-        this.maxPoolSize = maxPoolSize;
-    }
-
-    public int getQueueCapacity() {
-        return queueCapacity;
-    }
-
-    public void setQueueCapacity(int queueCapacity) {
-        this.queueCapacity = queueCapacity;
-    }
-
-    public int getKeepAliveSeconds() {
-        return keepAliveSeconds;
-    }
-
-    public void setKeepAliveSeconds(int keepAliveSeconds) {
-        this.keepAliveSeconds = keepAliveSeconds;
-    }
 }
